bb00bc3c64c46670934766ec803f0df990d3e363
temujin <tumukhan@icloud.com>
2026-03-31 17:52:42 +0200

    workig 0.0.1

--- main.py
+++ main.py
@@ -1,60 +1,66 @@
-def bits_to_text(bitstream):
-    # Ellenőrizni, hogy a bitfolyam 8-mal osztható legyen
-    if len(bitstream) % 8 != 0:
-        raise ValueError("A bitfolyam hossza nem osztható 8-cal!")
-
-    chars = []
-    for i in range(0, len(bitstream), 8):
-        byte = bitstream[i:i+8]          # 8 bites csoport
-        decimal = int(byte, 2)           # binárisból decimálissá
-        chars.append(chr(decimal))        # decimálisból karakter
-    return ''.join(chars)
+def convert_to_bytes(data):
+    return [ord(c) for c in data]
 
 
-def XOR(part1, part2):
-    partOne = ''
-    for _ in range(len(part1)):
-        if int(part1[_]) == int(part2[_]):
-            partOne += '1'
-        else:
-            partOne += '0'
-    return partOne
+def decode_from_bytes(byte_data): 
+    text = "".join(chr(c) for c in byte_data)
+    return text
 
-def run():
-    with open("original", 'rb') as rb:
-        data = rb.read()
+
+def make_erasure(byte_data):
+    size = len(byte_data)
+    by_how_much = size // 3
+
+    part1 = byte_data[:by_how_much] 
+    part2 = byte_data[by_how_much:by_how_much*2]
+    part3 = byte_data[by_how_much*2:]
+
+    return [part1, part2, part3]
+
+
+def XOR(data1, data2):
+    response = []
+    for i in range(len(data1)):
+        response.append(data1[i] ^ data2[i])
+
+    return response
+
+def complement(erause_data):
+    how_mach_take = len(erause_data[2]) - len(erause_data[1])
+
+    for i in range(how_mach_take):
+        erause_data[0].append(0)
+        erause_data[1].append(0)
+
+    return erause_data
+
+def make_parity(erause_data):
     
-    bit = ''.join(f'{b:08b}' for b in data)
-
-    # Output: '00000001111111111000000000000111'
-    
-    bit_length = len(bit)
-    bit_ = len(bit) // 3
-
-    part1 = bit[:bit_]
-    part2 = bit[bit_:bit_*2]
-    part3 = bit[bit_*2:]
-
-    print("part1", bits_to_text(part1))
-    print("part2", bits_to_text(part2))
-    print("part3", bits_to_text(part3))
-    
-    partOne = XOR(part1, part2)
-    print('partOne', bits_to_text(partOne))
-
-    perty = XOR(partOne, part3)
-    print("perty", bits_to_text(perty))
-
-    b = int(perty, 2).to_bytes(len(perty) // 8, byteorder="big")
-
-    with open("part1.bin", "wb") as wb:
-        wb.write(b)
-
-
-
-
-
+    erause_plus_data = complement(erause_data)
     
 
+    ep1 = XOR(erause_plus_data[0], erause_plus_data[1])
+    ep2 = XOR(ep1, erause_plus_data[2])
 
-run()
+    erause_data.append(ep2)
+
+    return  erause_data
+
+
+
+
+
+def run(data):    
+    byte_data = convert_to_bytes(data)
+
+    erasure_data = make_erasure(byte_data)
+
+    erasure_with_parity_data = make_parity(erasure_data)
+
+    return erasure_with_parity_data
+  
+
+if __name__ == "__main__":
+    print(run("Hello, world!"))
+
+
